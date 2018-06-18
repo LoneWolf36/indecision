10f8b0f11e5abf37c1212f435e1d47f3200efe47
LoneWolf36 <runefaizan36@gmail.com>
2018-06-19 00:22:31 +0530

    Playground

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -9,15 +9,16 @@ class Counter extends React.Component {
     };
   }
   componentDidMount() {
-    const stringCount = localStorage.getItem('count')
+    const stringCount = localStorage.getItem('count');
     const count = parseInt(stringCount, 10);
-    if(!isNaN(count)) {
-      this.setState(()=> ({ count }))
+
+    if (!isNaN(count)) {
+      this.setState(() => ({ count }));
     }
   }
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.count !== this.state.count) {
-      localStorage.setItem('count', this.state.count)
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
     }
   }
   handleAddOne() {
